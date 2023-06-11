95fcd98f03a9a6d1a62690e7d832a98f3dea2a73
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-06-11 16:43:01 +0000

    Added photos as a loop.

--- src/components/AlbumMaker.js
+++ src/components/AlbumMaker.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PhotoEntry from './PhotoEntry';
+import entries from './data/photo-entries.json'
 
 class AlbumMaker extends React.Component {
 
   render() {
 
+    console.log("Entries are:")
+    console.log(entries);
+
     return (
       <div className="flex-container">
-        <PhotoEntry src="berlin.jpg" location="Berlin, Germany" caption="Heart of Europe" />
-        <PhotoEntry src="beverly-hills.jpg" location="Beverly Hills, USA" caption="Southern California" />
-        <PhotoEntry src="maple-leaf.jpg" location="Toronto, Canada" caption="Autumn" />
-        <PhotoEntry src="wings.jpg" location="San Francisco, USA" caption="Wanderlust" />
-        <PhotoEntry src="hawaii.jpg" location="Kona, Hawaii, USA" caption="Deep Thought" />
-        <PhotoEntry src="london.jpg" location="London, UK" caption="Trafalgar Square" />
+        {
+        entries.itemlist.map((photo) => {
+          return <PhotoEntry key={photo.src} src={photo.src} location={photo.location} caption={photo.caption} /> }
+        )
+        }
+        {/* <PhotoEntry src="berlin.jpg" location="Berlin, Germany" caption="Heart of Europe" /> */}
       </div>
     )
 
