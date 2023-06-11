fe3083aeead39ee37ee296fd4cdd4ed787b7932a
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-06-11 15:40:50 +0000

    Added master.css.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import BannerImage from './components/BannerImage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <BannerImage />
         <div>
           <h1>Welcome to My Photojournal</h1>
           <h3>Nikhil Pandey</h3>
